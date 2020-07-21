2ffe2ac07b799a07e86575006ab48dafa8995d4b
rithikunsen <s.rithikun99@gmail.com>
2020-07-21 23:02:32 +0700

    update final project

--- app/src/main/java/com/example/malis_beauty/activity/LoginActivity.java
+++ app/src/main/java/com/example/malis_beauty/activity/LoginActivity.java
@@ -14,12 +14,14 @@ import com.example.malis_beauty.R;
 public class LoginActivity extends AppCompatActivity {
 
     Button button_login;
+    Button button_reset_pw;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         button_login = (Button) findViewById(R.id.btn_login);
+
 
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +30,6 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }
---
+++ app/src/main/java/com/example/malis_beauty/activity/ResetPasswordActivity.java
@@ -0,0 +1,18 @@
+package com.example.malis_beauty.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.malis_beauty.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset);
+
+    }
+}
